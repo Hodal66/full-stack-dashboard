4108ef7bdd23c06b637e40ec2bbe47d7e36bb922
hodal66 <mhthodol@gmail.com>
2023-11-30 23:01:19 +0200

    Now We Got User Id And Fetch All Other Content of the user

--- src/pages/users/User.jsx
+++ src/pages/users/User.jsx
@@ -19,46 +19,88 @@ query GetOneUser($id: ID!) {
 `
 
 function User() {
-  const [userDetail,setUserDetail] = useState('')
   let params = useParams();
   console.log(params.userID)
-const [fetchUserData,{data,error,loading}] = useLazyQuery(USER_QUERY_CONTAINER,{
-  variables:{
-    id:params.userID
+  const [userDetail,setUserDetail] = useState(params.userID)
+
+const [fetchUserData, { data, error, loading }] = useLazyQuery(
+  USER_QUERY_CONTAINER,
+  {
+    variables: {
+      id: userDetail,
+    },
   }
-});//
-if(loading) return <div>Data is Loading....</div>
-if(error) return <div>There are an error : {error}</div>
+); //
+if (loading) return <div>Data is Loading....</div>;
+if (error) return <div>There are an error : {error}</div>;
 
+console.log(fetchUserData);
+console.log("My Data:: ", data);
 
-console.log(fetchUserData());
+return (
+  <div>
+    <div className="h-screen bg-blue-50">
+      <DashboardHeader />
+      <div className="grid grid-cols-12 pt-20">
+        <div className="w-full col-span-3">
+          <Sidebar />
+        </div>
 
-  return (
-    <div>
-      <div className="h-screen bg-blue-50">
-        <DashboardHeader />
-        <div className="grid grid-cols-12 pt-20">
-          <div className="w-full col-span-3">
-            <Sidebar />
-          </div>
-          {
-            data &&  <div className="p-8  h-screen col-span-9">
-            <h1>We got a user now</h1>
-            <h1>Search User Right now!!</h1>
+        <div className="p-8  h-screen col-span-9 my-12">
+          <h1>We got a user now</h1>
+          <h1>Search User Right now!!</h1>
 
+       <div className="my-8">
+       <input
+            type="search"
+            placeholder="Please Enter userID.."
+            className="p-2 rounded-sm"
+            onChange={(event) => setUserDetail(event.target.value)}
+          />
+          <button
+            onClick={fetchUserData}
+            className="bg-blue-500  p-2 rounded-md "
+          >
+            Search Now
+          </button>
+       </div>
 
-            <input type="search" placeholder="Please search any user.." onChange={(event)=>setUserDetail(event.target.value)}/>
-            <button onClick={()=>fetchUserData()}>Search Now</button>
-
-            {userDetail && <div>New Data : {userDetail}</div>}
-
-          </div>
-          }
-         
+          {data && (
+            <div>
+              <p>
+                <span className="font-bold text-blue-800"> User ID : </span>
+                {data.getOneUser.id}
+              </p>
+              <p>
+                <span className="font-bold text-blue-800">
+                  Your Full Name is :
+                </span>
+                {data.getOneUser.fullName}
+              </p>
+              <p>
+                {" "}
+                <span className="font-bold text-blue-800">
+                  Your Email Address is :
+                </span>
+                {data.getOneUser.email}
+              </p>
+              <p>
+                <span className="font-bold text-blue-800">
+                  Time Created is :
+                </span>
+                {data.getOneUser.createdAt}
+              </p>
+              <p>
+                <span className="font-bold text-blue-800">Yor Token is : </span>
+                {data.getOneUser.token}
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default User;
